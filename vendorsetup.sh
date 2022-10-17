ab4675b8a37f14052b69c78d031be2f3e905a138
RahulPalXDA <RahulPal.XDA@gmail.com>
2022-10-17 17:42:16 +0530

    ginkgo: use GCamGOPrebuilt as prebuilt camera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
 rm -rf vendor/xiaomi/ginkgo
 rm -rf kernel/xiaomi/ginkgo
+rm -rf packages/apps/GCamGOPrebuilt
 rm -rf hardware/xiaomi/aidl/power-libperfmgr
 
 git clone --depth=1 https://github.com/GinkgoHub/android_vendor_xiaomi_ginkgo.git -b thirteen vendor/xiaomi/ginkgo
 git clone --depth=1 https://github.com/GinkgoHub/android_kernel_xiaomi_ginkgo.git -b thirteen kernel/xiaomi/ginkgo
+git clone --depth=1 https://github.com/GinkgoHub/android_packages_apps_GCamGOPrebuilt.git -b thirteen packages/apps/GCamGOPrebuilt
