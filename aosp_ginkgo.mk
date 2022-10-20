0356788e97fd8859852fdf1f8d79cd460a7d726c
RahulPalXDA <RahulPal.XDA@gmail.com>
2022-10-20 09:25:51 +0530

    adapt arrow device tree for @Project-Elixir * also clean arrow overlays

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -26,13 +26,17 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 # Inherit some common ArrowOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
-$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Inherit Some AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit Some elixir stuff
+$(call inherit-product, device/xiaomi/ginkgo/elixir.mk)
 
 # Device identifier
-PRODUCT_NAME := arrow_ginkgo
+PRODUCT_NAME := aosp_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-DEVICE_MAINTAINER := Adithya (ghostrider_reborn)
