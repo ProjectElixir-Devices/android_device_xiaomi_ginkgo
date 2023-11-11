098ef3ce56ceecd1cebebd8855274a304e7dd41c
Arian <arian.kulmer@web.de>
2023-11-11 20:18:11 +0100

    ginkgo: Provide a separate recovery fstab
    Imported from LA.UM.9.1.r1-14700-SMxxx0.QSSI13.0 without slotselect, vbmeta_system and f2fs on data.
    Our main fstab sets keydirectory=/metadata/vold/metadata_encryption for userdata, however when having this in recovery fstab, userdata does not get formatted correctly, leading to a loop in rescue party.
    
    Change-Id: I2ad7dbd941744c6bec14bc91d1a90245ad77b584

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # Releasetools
