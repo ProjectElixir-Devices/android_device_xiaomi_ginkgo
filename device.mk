DerpFest-ginkgo/device_xiaomi_ginkgo

ginkgo: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

device.mk
@@ -360,6 +360,7 @@ PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-full \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
 
 PRODUCT_PACKAGES += \

ProjectElixir-Devices/android_device_xiaomi_ginkgo

device.mk
@@ -318,7 +318,8 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QTI VNDK Framework Detect
 PRODUCT_PACKAGES += \